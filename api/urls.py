c1f278a801bcc5a1a132107008538a4efa26cd5b
Apfirebolt <aspper20@gmail.com>
2024-06-21 23:40:43 +0530

    added a sample qr code generator

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from . views import ListCustomUsersApiView, CreateCustomUserApiView, ListItemApiView, DetailItemApiView
+from . views import ListCustomUsersApiView, CreateCustomUserApiView, ListItemApiView, DetailItemApiView, QRCodeApiView
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -12,4 +12,5 @@ urlpatterns = [
     path('users', ListCustomUsersApiView.as_view(), name='list-users'),
     path('items', ListItemApiView.as_view(), name='list-items'),
     path('items/<int:id>', DetailItemApiView.as_view(), name='detail-item'),
+    path('qrcode', QRCodeApiView.as_view(), name='qrcode'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,7 +1,10 @@
+import qrcode
 from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveAPIView
+from rest_framework.views import APIView
 from . serializers import ListCustomUserSerializer, CustomUserSerializer, CustomTokenObtainPairSerializer, ListItemsSerializer
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import filters
+from rest_framework.response import Response
 from django.views.decorators.cache import cache_page
 from django.utils.decorators import method_decorator
 from rest_framework.throttling import UserRateThrottle, AnonRateThrottle
@@ -35,6 +38,7 @@ class ListItemApiView(ListAPIView):
     filterset_fields = ['title', 'brand']
     ordering_fields = ['price']
     search_fields = ['title', 'brand']
+    permission_classes = [IsAuthenticated]
 
     @method_decorator(cache_page(60 * 60)) 
     def get(self, request, *args, **kwargs):
@@ -59,6 +63,19 @@ class DetailItemApiView(RetrieveAPIView):
     serializer_class = ListItemsSerializer
     queryset = Item.objects.all()
     lookup_field = 'id'
+
+
+class QRCodeApiView(APIView):
+
+    def get(self, request, *args, **kwargs):
+        data = "https://www.apgiiit.com"  # Replace with your data
+
+        qr = qrcode.make(data, version=1, box_size=10)  # Optional parameters
+        
+        # save qg code image in a new media folder
+        qr.save('media/qrcode.png')
+        return Response({'message': 'This is a QR code API view'})
+    
 
 
 
